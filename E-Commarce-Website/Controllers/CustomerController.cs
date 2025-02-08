e9b61683de495a3e3504a5e51f97909a0d23aa24
saurabhthombare <125489427+saurabhthombare@users.noreply.github.com>
2025-02-08 11:46:53 +0530

    Create a UI and additional field

---
+++ E-Commarce-Website/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commarce_Website.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
